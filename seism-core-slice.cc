f88099f0973654093f77ed67358691172ecd2751
Frank T Willmore <Frank.Willmore@hdfgroup.org>
2016-12-19 11:32:57 -0600

    added call to set_all_collective

--- seism-core-slice.cc
+++ seism-core-slice.cc
@@ -47,11 +47,6 @@ void precreate_0
   assert(H5Pset_libver_bounds(fapl, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST) >=
          0);
 
-#if (H5_VERS_MAJOR == 1 && H5_VERS_MINOR >= 10) 
-  assert(H5Pset_all_coll_metadata_ops(fapl, true) >=0 );
-  printf("Precreate_0 setting all_coll_meta_data_ops true\n");
-#endif
-
   hid_t file = H5Fcreate(fname.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, fapl);
   assert(file >= 0);
   hid_t dset = H5Dcreate(file, CHUNKED_DSET_NAME, H5T_IEEE_F32LE, fspace,
@@ -242,6 +237,11 @@ int main(int argc, char** argv)
       info = MPI_INFO_NULL;
     }
   assert(H5Pset_fapl_mpio(fapl, MPI_COMM_WORLD, info) >= 0);
+
+#if (H5_VERS_MAJOR == 1 && H5_VERS_MINOR >= 10) 
+  assert(H5Pset_all_coll_metadata_ops(fapl, 1) >=0 );
+  printf("setting all_coll_meta_data_ops true\n");
+#endif
 
   // file handle and name for file which will be created
   string fname = "seism-test.h5";
