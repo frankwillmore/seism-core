ae795a404cc40f517d9ac4d65ce2a320a750d008
Allen Byrne <byrn@hdfgroup.org>
2017-08-28 14:51:49 -0500

    Add Visual Studio 2017 option

--- bb_runscripts/bbhdf5mpi/runhdf5trunk.sh
+++ bb_runscripts/bbhdf5mpi/runhdf5trunk.sh
@@ -1,12 +1,73 @@
-mkdir hdfsrc
-cd hdfsrc
-git clone --branch develop 'ssh://git@bitbucket.hdfgroup.org:7999/~byrn/hdf5_adb.git' . --progress
-cd ..
+# PLATFORM list
+# "fedora",
+# "debian",
+# "ubuntu",
+# "opensuse",
+# "centos6",
+# "centos7",
+# "cygwin",
+# "ppc64",
+# "osx109",
+# "osx1010",
+# "osx1011",
+
+USAGE()
+{
+cat << EOF
+Usage: $0 <platform>
+   Run tests for <platform>
+
+EOF
+
+}
+
+
+if [ $# != 1 ]; then
+    USAGE
+    exit 1
+fi
+
+PLATFORM="$1"
+
+HOST='206.221.145.51'
+DIRN='/mnt/ftp/pub/outgoing/QATEST/'
+
+GENERATOR='Unix Makefiles'
+CONFIG=StdMPITrunk
+PRODUCT=hdf5perf
+OSSIZE=64
+
 mkdir build
 cd build
-python ../doftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ hdf5trunk/ . bbtconf.json
-python ../doFilesftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ fedora25 fedora StdMPIShar bbparams ctest DTP/extra bbtconf.json
-python ../doCTconfig.py bbtconf.json StdMPIShar bbparams ctest fedora schedule C Fortran Java MPI
-python ../doCTrun.py bbtconf.json StdMPIShar bbparams ctest fedora 'Unix Makefiles'
-python ../doCTpackage.py bbtconf.json StdMPIShar bbparams ctest fedora25 fedora 'Unix Makefiles' local-worker
-python ../dobtftpup.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ fedora25 fedora StdMPIShar bbparams ctest 'Unix Makefiles' hdf5trunk bbtconf.json
+#
+mkdir hdfsrc
+cd hdfsrc
+#git clone --branch master 'ssh://git@bitbucket.hdfgroup.org:7999/hdffv/hdf5.git' . --progress
+git clone --branch master 'https://git@bitbucket.hdfgroup.org/scm/hdffv/hdf5.git' . --progress
+cd ..
+python ./doftp.py $HOST $DIRN scripts . bbtconf.json
+#
+mkdir autotools
+cd autotools
+python ../../doftp.py $HOST $DIRN hdf5trunk/ . bbtconf.json
+python ../../doftp.py $HOST $DIRN scripts . doFilesftp.py
+python ../../doftp.py $HOST $DIRN scripts . doATconfig.py
+python ../../doftp.py $HOST $DIRN scripts . doATmake.py
+python ../../doftp.py $HOST $DIRN scripts . doATcheck.py
+python ../../doftp.py $HOST $DIRN scripts . doATinstall.py
+python ../../doftp.py $HOST $DIRN scripts . doATinstallcheck.py
+python ../../doftp.py $HOST $DIRN scripts . doATpackage.py
+python ../../doftp.py $HOST $DIRN scripts . dobtftpup.py
+python ../../doftp.py $HOST $DIRN scripts . doATuninstall.py
+#
+python ./doFilesftp.py $HOST $DIRN $PLATFORM $PLATFORM $CONFIG bbparams autotools DTP/extra bbtconf.json
+python ./doATconfig.py bbtconf.json $CONFIG bbparams $PLATFORM schedule C Fortran Java
+python ./doATmake.py bbtconf.json $CONFIG bbparams $PLATFORM schedule C Fortran Java
+python ./doATcheck.py bbtconf.json $CONFIG bbparams $PLATFORM schedule C Fortran Java
+python ./doATinstall.py bbtconf.json $CONFIG bbparams $PLATFORM schedule C Fortran Java
+python ./doATinstallcheck.py bbtconf.json $CONFIG bbparams $PLATFORM schedule C Fortran Java
+python ./doATpackage.py bbtconf.json $CONFIG bbparams $PLATFORM schedule
+python ./dobtftpup.py $HOST $DIRN $PLATFORM $OSSIZE $PLATFORM $CONFIG bbparams autotools "$GENERATOR" hdf5trunk bbtconf.json
+python ./doATuninstall.py bbtconf.json $CONFIG bbparams $PLATFORM schedule C Fortran Java
+cd ../..
+
--- bb_runscripts/bbperf/runperf.sh
+++ bb_runscripts/bbperf/runperf.sh
@@ -1,30 +1,88 @@
-mkdir hdfsrc
-cd hdfsrc
-git clone --branch master 'ssh://git@bitbucket.hdfgroup.org:7999/~byrn/performance_adb.git' . --progress
-cd ..
+# PLATFORM list
+# "fedora",
+# "debian",
+# "ubuntu",
+# "opensuse",
+# "centos6",
+# "centos7",
+# "cygwin",
+# "ppc64",
+# "osx109",
+# "osx1010",
+# "osx1011",
+
+USAGE()
+{
+cat << EOF
+Usage: $0 <platform>
+   Run tests for <platform>
+
+EOF
+
+}
+
+
+if [ $# != 1 ]; then
+    USAGE
+    exit 1
+fi
+
+PLATFORM="$1"
+
+HOST='206.221.145.51'
+DIRN='/mnt/ftp/pub/outgoing/QATEST/'
+
+GENERATOR='Unix Makefiles'
+CONFIG=StdMPITrunk
+PRODUCT=hdf5perf
+OSSIZE=64
+
 mkdir build
 cd build
-python ../doftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ hdf5perf/ . 19bbtconf.json
-python ../dobtftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ centos7 centos7 StdMPITrunk bbparams autotools 'Unix Makefiles' insbin 19bbtconf.json
-python ../dobtuncompress.py centos7 centos7 StdMPITrunk bbparams autotools 'Unix Makefiles' insbin 19bbtconf.json
-python ../doatlin.py insbin 19bbtconf.json
+#
+mkdir hdfsrc
+cd hdfsrc
+#git clone --branch master 'ssh://git@bitbucket.hdfgroup.org:7999/hdffv/performance.git' . --progress
+git clone --branch master 'https://git@bitbucket.hdfgroup.org/scm/hdffv/performance.git' . --progress
+cd ..
+python ../doftp.py $HOST $DIRN scripts . bbtconf.json
+#
+mkdir autotools
+cd autotools
+python ../../doftp.py $HOST $DIRN hdf5perf/ . bbtconf.json
+python ../../doftp.py $HOST $DIRN scripts . doDistributeGet.py
+python ../../doftp.py $HOST $DIRN scripts . dobtftp.py
+python ../../doftp.py $HOST $DIRN scripts . dobtuncompress.py
+python ../../doftp.py $HOST $DIRN scripts . doatlin.py
+python ../../doftp.py $HOST $DIRN scripts . doFilesftp.py
+python ../../doftp.py $HOST $DIRN scripts . doATconfig.py
+python ../../doftp.py $HOST $DIRN scripts . doATmake.py
+python ../../doftp.py $HOST $DIRN scripts . doATcheck.py
+#
+python ./doDistributeGet.py $HOST $DIRN $PLATFORM $PLATFORM $CONFIG bbparams bbtconf.json
+python ./dobtftp.py $HOST $DIRN $PLATFORM $OSSIZE $PLATFORM $CONFIG bbparams autotools "$GENERATOR" insbin bbtconf.json
+python ./dobtuncompress.py $PLATFORM $OSSIZE $PLATFORM $CONFIG bbparams autotools "$GENERATOR" insbin bbtconf.json
+python ./doatlin.py insbin bbtconf.json
 #combust_io test
-python ../doFilesftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ centos7 centos7 StdMPITrunk combust_io autotools DTP/extra 19bbtconf.json
-python ../doATconfig.py 19bbtconf.json StdMPITrunk combust_io centos7 kituo-slave C Fortran Java MPI
-python ../doATmake.py 19bbtconf.json StdMPITrunk combust_io centos7 kituo-slave C Fortran Java MPI
-python ../doATcheck.py 19bbtconf.json StdMPITrunk combust_io centos7 kituo-slave C Fortran Java MPI
+python ./doFilesftp.py $HOST $DIRN $PLATFORM $PLATFORM $CONFIG combust_io autotools DTP/extra bbtconf.json
+python ./doATconfig.py bbtconf.json $CONFIG combust_io $PLATFORM schedule C Fortran MPI
+python ./doATmake.py bbtconf.json $CONFIG combust_io $PLATFORM schedule C Fortran MPI
+python ./doATcheck.py bbtconf.json $CONFIG combust_io $PLATFORM schedule C Fortran MPI
 #h5core test
-python ../doFilesftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ centos7 centos7 StdMPITrunk h5core autotools DTP/extra 19bbtconf.json
-python ../doATconfig.py 19bbtconf.json StdMPITrunk h5core centos7 kituo-slave C Fortran Java MPI
-python ../doATmake.py 19bbtconf.json StdMPITrunk h5core centos7 kituo-slave C Fortran Java MPI
-python ../doATcheck.py 19bbtconf.json StdMPITrunk h5core centos7 kituo-slave C Fortran Java MPI
+python ./doFilesftp.py $HOST $DIRN $PLATFORM $PLATFORM $CONFIG h5core autotools DTP/extra bbtconf.json
+python ./doATconfig.py bbtconf.json $CONFIG h5core $PLATFORM schedule C Fortran MPI
+python ./doATmake.py bbtconf.json $CONFIG h5core $PLATFORM schedule C Fortran MPI
+python ./doATcheck.py bbtconf.json $CONFIG h5core $PLATFORM schedule C Fortran MPI
 #h5perf test
-python ../doFilesftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ centos7 centos7 StdMPITrunk h5perf autotools DTP/extra 19bbtconf.json
-python ../doATconfig.py 19bbtconf.json StdMPITrunk h5perf centos7 kituo-slave C Fortran Java MPI
-python ../doATmake.py 19bbtconf.json StdMPITrunk h5perf centos7 kituo-slave C Fortran Java MPI
-python ../doATcheck.py 19bbtconf.json StdMPITrunk h5perf centos7 kituo-slave C Fortran Java MPI
+python ./doFilesftp.py $HOST $DIRN $PLATFORM $PLATFORM $CONFIG h5perf autotools DTP/extra bbtconf.json
+python ./doATconfig.py bbtconf.json $CONFIG h5perf $PLATFORM schedule C Fortran Java MPI
+python ./doATmake.py bbtconf.json $CONFIG h5perf $PLATFORM schedule C Fortran Java MPI
+python ./doATcheck.py bbtconf.json $CONFIG h5perf $PLATFORM schedule C Fortran Java MPI
 #seism-core test
-python ../doFilesftp.py jam.hdfgroup.uiuc.edu /mnt/ftp/pub/outgoing/QATEST/ centos7 centos7 StdMPITrunk seism-core autotools DTP/extra 19bbtconf.json
-python ../doATconfig.py 19bbtconf.json StdMPITrunk seism-core centos7 kituo-slave C Fortran Java MPI
-python ../doATmake.py 19bbtconf.json StdMPITrunk seism-core centos7 kituo-slave C Fortran Java MPI
-python ../doATcheck.py 19bbtconf.json StdMPITrunk seism-core centos7 kituo-slave C Fortran Java MPI
+python ./doFilesftp.py $HOST $DIRN $PLATFORM $PLATFORM $CONFIG seism-core autotools DTP/extra bbtconf.json
+python ./doATconfig.py bbtconf.json $CONFIG seism-core $PLATFORM schedule C Fortran MPI
+python ./doATmake.py bbtconf.json $CONFIG seism-core $PLATFORM schedule C Fortran MPI
+python ./doATcheck.py bbtconf.json $CONFIG seism-core $PLATFORM schedule C Fortran MPI
+#
+cd ../..
+
